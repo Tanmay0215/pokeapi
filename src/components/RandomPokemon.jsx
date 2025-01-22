dcc04251e93c54f495080f023d8250657014c8da
Tanmay0215 <tanmaygupta.0215@gmail.com>
2025-01-22 22:59:37 +0530

    random pokemon component

---
+++ src/components/RandomPokemon.jsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react'
+
+function RandomPokemon() {
+  const [pokemon, setPokemon] = useState({})
+
+  useEffect(() => {
+    let i = Math.floor(Math.random() * 200) + 1
+    console.log(i)
+    const fetchPokemon = async () => {
+      try {
+        const response = await fetch(
+          'https://pokeapi.co/api/v2/pokemon-form/' + i
+        )
+        const data = await response.json()
+        // console.log(data.pokemon.name, data.sprites.front_default)
+        setPokemon({ name: data.pokemon.name, sprite: data.sprites.front_default })
+      } catch (err) {
+        console.log(err)
+      }
+    }
+    fetchPokemon()
+  }, [])
+
+  return (
+    <div>
+      <img src={pokemon.sprite} alt={pokemon.name} className='h-52' />
+    </div>
+  )
+}
+
+export default RandomPokemon
