5c5d260f0aa885fed4ab8131464a8e603b69916e
Tanmay0215 <tanmaygupta.0215@gmail.com>
2025-01-23 12:49:21 +0530

    added footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,20 @@ import Home from './pages/Home.jsx'
 import Random from './pages/Random.jsx'
 import Type from './pages/Type.jsx'
 import NotFound from './pages/NotFound.jsx'
+import Navbar from './components/Navbar.jsx'
+import Footer from './components/Footer.jsx'
 
 function App() {
   return (
-    <div className='min-h-screen font-manrope'>
+    <div className='min-h-screen font-manrope bg-zinc-100'>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/random" element={<Random />} />
           <Route path="/type/:id" element={<Type />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
+        <Footer />
     </div>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom"
+
+function Footer() {
+  return (
+    <div className="text-center p-4 text-sm">
+        This project was developer using{' '}
+        <Link to="https://pokeapi.co/">PokeAPI</Link>
+        {/* <img src="https://pokeapi.co/static/pokeapi_256.3fa72200.png" alt="pokeapi" /> */}
+    </div>
+  )
+}
+
+export default Footer
