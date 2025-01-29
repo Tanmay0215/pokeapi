10eaa6b1482209b9ffe94f7d7814da1a5eff2954
Tanmay0215 <tanmaygupta.0215@gmail.com>
2025-01-29 13:18:33 +0530

    improved random page

--- src/pages/Random.jsx
+++ src/pages/Random.jsx
@@ -1,13 +1,80 @@
-import RandomPokemon from '../components/RandomPokemon'
+import { useState, useEffect } from 'react'
+import { TypeColor } from '../context/Types'
+import Loader from '../components/Loader'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
 
-function Random() {
-  return (
-    <div className="flex flex-col bg-zinc-100">
-      <div className='flex gap-3 justify-center items-center h-full'>
-        <RandomPokemon />
+function RandomPokemon() {
+  const [pokemon, setPokemon] = useState({})
+  const [generate, setgenerate] = useState(false)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    let i = Math.floor(Math.random() * 200) + 1
+    const fetchPokemon = async () => {
+      try {
+        const response = await fetch(
+          'https://pokeapi.co/api/v2/pokemon-form/' + i
+        )
+        const data = await response.json()
+        setPokemon({
+          name: data.name,
+          sprite: data.sprites.front_default,
+          types: data.types.map((type) => type.type.name),
+        })
+        setLoading(false)
+        // setPokemon(localStorage.getItem('allPokemons')[i])
+        // console.log(localStorage.getItem('allPokemons').map((pokemon) => pokemon))
+      } catch (err) {
+        console.log(err)
+      }
+    }
+    fetchPokemon()
+  }, [generate])
+
+  if (loading) {
+    return (
+      <div className="text-center bg-white flex flex-col gap-10 justify-center items-center h-screen p-5">
+        <Loader />
       </div>
+    )
+  }
+
+  return (
+    <div>
+      <Navbar />
+      <div className='text-center flex justify-center items-center p-5'>
+      <div
+        className={`flex flex-col items-center rounded-md ${
+          TypeColor[pokemon.types && pokemon.types[0]]
+          } p-5 bg-opacity-75 max-w-lg`}
+          >
+        <img src={pokemon.sprite} alt={pokemon.name} className="h-52" />
+        <h1 className="capitalize text-2xl -mt-5 p-5 font-medium">
+          {pokemon.name}
+        </h1>
+        <div className="flex gap-2">
+          {pokemon.types &&
+            pokemon.types.map((type, index) => (
+              <div
+              key={index}
+              className={`px-3 py-2 rounded-lg text-sm shadow-md bg-opacity-40 capitalize ${TypeColor[type]}`}
+              >
+                {type}
+              </div>
+            ))}
+        </div>
+        <button
+          onClick={() => setgenerate(!generate)}
+          className="mt-5 bg-white bg-opacity-30 rounded-lg py-2 px-5"
+          >
+          Generate New
+        </button>
+      </div>
+          </div>
+      <Footer />
     </div>
   )
 }
 
-export default Random
+export default RandomPokemon
