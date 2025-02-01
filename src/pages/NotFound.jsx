5dfe5629f76597ac57e1a82cc68764ab88d7352a
Tanmay0215 <tanmaygupta.0215@gmail.com>
2025-02-01 22:10:12 +0530

    update not found

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,9 +1,10 @@
 import { Link } from 'react-router-dom';
+import notfound from '../assets/404.png';
 
 function NotFound() {
   return (
     <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100 text-center">
-      <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png" alt="Pikachu" className="w-32 h-32 mb-4" />
+      <img src={notfound} alt="Pikachu" className="w-32 h-32 mb-4" />
       <h1 className="text-4xl font-bold mb-2">404</h1>
       <p className="text-xl mb-4">Oops! The page you&apos;re looking for doesn&apos;t exist.</p>
       <Link to="/" className="text-blue-500 underline">Go back to Home</Link>
