a245fc0f8e3aef3bfee1c5c284e3f85cf78facaf
Tanmay0215 <tanmaygupta.0215@gmail.com>
2025-02-28 22:45:53 +0530

    update pokemon card and random page

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,43 +1,29 @@
-import { useNavigate } from 'react-router-dom'
 import { TypeColor } from '../context/Types'
-import add from '../assets/add.svg'
-import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
-const PokemonCard = ({ pokemon }, addToWishList) => {
+const PokemonCard = ({ pokemon }) => {
   const navigate = useNavigate()
   return (
     <div
       className={`${
         TypeColor[pokemon.types[0]]
-      } bg-opacity-50 p-4 rounded-lg flex flex-col items-center shadow-md transform transition duration-500 hover:scale-105`}
+      } bg-opacity-50 p-4 rounded-lg flex flex-col items-center shadow-md transform transition duration-500 md:hover:scale-105`}
     >
-      <img
-        src={add}
-        alt="add_icon"
-        onClick={() => addToWishList(pokemon.id - 1)}
-        className="cursor-pointer h-5 bg-white/20 opacity-60 absolute top-0 right-0 m-2 rounded-full"
-      />
-      <img src={pokemon.sprite} alt={pokemon.name} className="h-32"/>
+      <img src={pokemon.sprite} alt={pokemon.name} className="h-32" onClick={()=>navigate(`/pokemon/${pokemon.id}`)} />
       <div className="text-md p-2 uppercase font-semibold">{pokemon.name}</div>
-      <div className="flex flex-wrap justify-center capitalize">
+      <div className="flex flex-wrap capitalize">
         {pokemon.types.map((type, index) => {
           return (
-            <Link
+            <div
               key={index}
-              to={`/type/${type}`}
-              className={`${TypeColor[type]} px-3 py-1 m-1 rounded-full text-xs bg-opacity-40`}
+              onClick={()=>navigate(`/type/${type}`)}
+              className={`${TypeColor[type]} px-3 py-1 m-1 rounded-full text-xs bg-opacity-40 cursor-pointer`}
             >
               {type}
-            </Link>
+            </div>
           )
         })}
       </div>
-      <button
-        onClick={() => navigate(`/pokemon/${pokemon.id}`)}
-        className="bg-opacity-20 text-xs rounded-full py-2 px-5 mt-2"
-      >
-        View More
-      </button>
     </div>
   )
 }
--- src/pages/Random.jsx
+++ src/pages/Random.jsx
@@ -3,6 +3,7 @@ import { TypeColor } from '../context/Types'
 import Loader from '../components/Loader'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import PokemonCard from '../components/PokemonCard'
 
 function RandomPokemon() {
   const [pokemon, setPokemon] = useState({})
@@ -10,13 +11,14 @@ function RandomPokemon() {
   const [loading, setLoading] = useState(true)
 
   const fetchPokemon = async () => {
-    let i = Math.floor(Math.random() * 200) + 1
     try {
-      const data = JSON.parse(localStorage.getItem('allPokemons'))[i]
+      const data = JSON.parse(localStorage.getItem('allPokemons'))
+      let i = Math.floor(Math.random() * data.length) + 1
       setPokemon({
-        name: data.name,
-        sprite: data.sprite,
-        types: data.types,
+        name: data[i].name,
+        sprite: data[i].sprite,
+        types: data[i].types,
+        stats: data[i].stats,
       })
       setLoading(false)
     } catch (err) {
@@ -39,34 +41,16 @@ function RandomPokemon() {
   return (
     <div>
       <Navbar />
-      <div className="text-center flex justify-center items-center p-5">
-        <div
-          className={`flex flex-col items-center rounded-md ${
-            TypeColor[pokemon.types && pokemon.types[0]]
-          } p-5 bg-opacity-75 max-w-lg`}
+      <div className="text-center flex flex-col justify-center items-center p-5">
+      <PokemonCard
+        pokemon={pokemon}
+      />
+        <button
+          onClick={() => setgenerate(!generate)}
+          className="mt-5 bg-white bg-opacity-20 text-sm rounded-lg py-2 px-5"
         >
-          <img src={pokemon.sprite} alt={pokemon.name} className="h-52" />
-          <h1 className="capitalize text-2xl -mt-8 p-5 font-medium">
-            {pokemon.name}
-          </h1>
-          <div className="flex gap-2">
-            {pokemon.types &&
-              pokemon.types.map((type, index) => (
-                <div
-                  key={index}
-                  className={`px-3 py-2 rounded-lg text-sm shadow-md bg-opacity-40 capitalize ${TypeColor[type]}`}
-                >
-                  {type}
-                </div>
-              ))}
-          </div>
-          <button
-            onClick={() => setgenerate(!generate)}
-            className="mt-5 bg-white bg-opacity-20 text-sm rounded-lg py-2 px-5"
-          >
-            Generate New
-          </button>
-        </div>
+          Generate New
+        </button>
       </div>
       <Footer />
     </div>
