30566f86464e566768deb04900badb84102c8097
Tanmay0215 <tanmaygupta.0215@gmail.com>
2025-04-04 23:43:51 +0530

    add shadowpokemon

--- src/pages/ShadowPokemon.jsx
+++ src/pages/ShadowPokemon.jsx
@@ -1,4 +1,6 @@
 import { useState, useEffect } from 'react'
+import Loader from '../components/Loader'
+import Navbar from '../components/Navbar'
 
 const ShadowPokemon = () => {
   const [shadowPokemon, setShadowPokemon] = useState(null)
@@ -6,16 +8,23 @@ const ShadowPokemon = () => {
   const [brightness, setBrightness] = useState('0%')
 
   const checkGuess = () => {
-    if (guess.toLowerCase() === shadowPokemon.name.toLowerCase()) {
+    if (guess.toLowerCase() === shadowPokemon.name) {
       setBrightness('100%')
       setTimeout(() => {
         alert('Correct!')
         setGuess('')
         loadNewPokemon()
-      }, 500) // Delay the alert and loading of the next Pokémon
+      }, 500)
     } else {
       alert('Incorrect!')
     }
+  }
+
+  const revealAnswer = () => {
+    alert(`The answer is ${shadowPokemon.name}`)
+    setGuess('')
+    setBrightness('100%')
+    // loadNewPokemon()
   }
 
   const loadNewPokemon = () => {
@@ -33,28 +42,45 @@ const ShadowPokemon = () => {
     loadNewPokemon()
   }, [])
 
+  if (!shadowPokemon) {
+    return (
+      <div className="flex justify-center items-center">
+        <Loader />
+      </div>
+    )
+  }
+
   return (
-    <div className="flex flex-col items-center justify-center">
-      <h1 className="text-3xl font-bold mb-4">Who's that Shadow Pokémon?</h1>
-      <img
-        src={shadowPokemon.sprite}
-        alt={shadowPokemon.name}
-        draggable="false"
-        className={`mb-4 brightness-${brightness}`}
-      />
-      <input
-        type="text"
-        value={guess}
-        onChange={(e) => setGuess(e.target.value)}
-        className="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mb-4"
-        placeholder="Enter Pokémon name"
-      />
-      <button
-        onClick={checkGuess}
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-      >
-        Check
-      </button>
+    <div>
+      <Navbar />
+      <div className="flex flex-col items-center justify-center">
+        <h1 className="text-3xl font-bold">Who's that Pokémon?</h1>
+        <img
+          src={shadowPokemon.sprite}
+          draggable="false"
+          className="size-40"
+          style={{ filter: `brightness(${brightness})` }}
+        />
+        <input
+          type="text"
+          value={guess}
+          onChange={(e) => setGuess(e.target.value)}
+          className="rounded py-2 px-3 text-gray-700"
+          placeholder="Enter Pokémon name"
+        />
+        <button
+          onClick={checkGuess}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Check
+        </button>
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-3"
+          onClick={revealAnswer}
+        >
+          RevealAnswer
+        </button>
+      </div>
     </div>
   )
 }
